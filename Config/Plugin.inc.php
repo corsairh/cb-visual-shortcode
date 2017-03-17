c35aad171564c1b579f60d5f105896f510639dec
Ahmed Said <xpointer@hotmail.com>
2017-03-17 02:01:07 +0200

    Write Production configuration

--- Config/Plugin.inc.php
+++ Config/Plugin.inc.php
@@ -16,10 +16,10 @@ return array
     
         'store' => array
         (
-            'apiEndPoint' => 'http://cbspoint.com/cbstore',
+            'apiEndPoint' => 'http://www.cbspoint.com/cbstore',
             'apiAddons' => array
             (
-                'url' => 'http://cbspoint.com/plugins/visual-shortcode-community-edition/feed/?items=addons',
+                'url' => 'http://www.cbspoint.com/plugins/visual-shortcode-community-edition/feed/?items=addons',
                 'expires' => 864000, /* Retrieve Every Ten days */
             ),
         ),
